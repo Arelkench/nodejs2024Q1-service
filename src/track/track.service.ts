e3e63f64439fb8706c845a8e0a5eae9895f68e3b
Arelkench <nefera2801@gmail.com>
2024-03-11 16:35:38 +0100

    add track functionality

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,26 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpCode,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { UpdateTrackDto } from './dto/update-track.dto';
+import { DbService } from '../db/db.service';
+import { Track } from '../db/models/track.model';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class TrackService {
+  constructor(private dbService: DbService) {}
   create(createTrackDto: CreateTrackDto) {
-    return 'This action adds a new track';
+    const track: Track = {
+      id: uuidv4(),
+      ...createTrackDto,
+    };
+    this.dbService.tracks.push(track);
+    return track;
   }
-
   findAll() {
-    return `This action returns all track`;
+    return this.dbService.tracks;
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} track`;
+  findOne(id: string) {
+    if (!id) {
+      throw new BadRequestException('Invalid user ID provided');
+    }
+    const track = this.dbService.tracks.find((track) => track.id === id);
+    if (!track) {
+      throw new NotFoundException('Track with this ID not found');
+    }
+    return track;
   }
-
-  update(id: number, updateTrackDto: UpdateTrackDto) {
-    return `This action updates a #${id} track`;
+  update(id: string, updateTrackDto: UpdateTrackDto) {
+    const track = this.dbService.tracks.find((track) => track.id === id);
+    if (!track) {
+      throw new NotFoundException('Track with this ID not found');
+    }
+    return Object.assign(track, updateTrackDto);
   }
-
-  remove(id: number) {
-    return `This action removes a #${id} track`;
+  remove(id: string) {
+    if (!id) {
+      throw new BadRequestException('Invalid track ID provided');
+    }
+    const track = this.dbService.tracks.find((track) => track.id === id);
+    if (!track) {
+      throw new NotFoundException('Track with this ID not found');
+    }
+    const trackIndex = this.dbService.tracks.indexOf(track);
+    this.dbService.tracks.splice(trackIndex, 1);
   }
 }
