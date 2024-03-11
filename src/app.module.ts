ce3a9f36f5a3a65cd98d09a2f32ba7224fefb85c
Arelkench <nefera2801@gmail.com>
2024-03-11 01:32:17 +0100

    add db and user functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { PlayersModule } from './players/players.module';
 import { ArtistModule } from './artist/artist.module';
 import { TrackModule } from './track/track.module';
 import { AlbumModule } from './album/album.module';
 import { FavoritesModule } from './favorites/favorites.module';
 import { DbModule } from './db/db.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, ArtistModule, TrackModule, AlbumModule, FavoritesModule, DbModule],
+  imports: [UserModule, ArtistModule, TrackModule, AlbumModule, FavoritesModule, DbModule, PlayersModule],
   controllers: [AppController],
   providers: [AppService],
 })
