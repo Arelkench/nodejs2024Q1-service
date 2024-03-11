cd0f908d21cc8af930e2f8c99b8ac2d10336564e
Arelkench <nefera2801@gmail.com>
2024-03-11 17:07:48 +0100

    add artist functionality

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ArtistService } from './artist.service';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { UpdateArtistDto } from './dto/update-artist.dto';
 
 @Controller('artist')
-export class ArtistController {}
+export class ArtistController {
+  constructor(private readonly artistService: ArtistService) {}
+
+  @Post()
+  create(@Body(ValidationPipe) createArtistDto: CreateArtistDto) {
+    return this.artistService.create(createArtistDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.artistService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    const artist = this.artistService.findOne(id);
+    return artist;
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body(ValidationPipe) updateArtistDto: UpdateArtistDto,
+  ) {
+    return this.artistService.update(id, updateArtistDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.artistService.remove(id);
+  }
+}
--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,4 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { DbService } from '../db/db.service';
+import { v4 as uuidv4 } from 'uuid';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { Artist } from '../db/models/artist.model';
+import { UpdateArtistDto } from './dto/update-artist.dto';
 
 @Injectable()
-export class ArtistService {}
+export class ArtistService {
+  constructor(private dbService: DbService) {}
+  create(createArtistDto: CreateArtistDto) {
+    const artist: Artist = {
+      id: uuidv4(),
+      ...createArtistDto,
+    };
+    this.dbService.artists.push(artist);
+    return artist;
+  }
+  findAll() {
+    return this.dbService.artists;
+  }
+  findOne(id: string) {
+    if (!id) {
+      throw new BadRequestException('Invalid artist ID provided');
+    }
+    const artist = this.dbService.artists.find((artist) => artist.id === id);
+    if (!artist) {
+      throw new NotFoundException('Artist with this ID not found');
+    }
+    return artist;
+  }
+  update(id: string, updateArtistDto: UpdateArtistDto) {
+    const artist = this.dbService.artists.find((artist) => artist.id === id);
+    if (!artist) {
+      throw new NotFoundException('Artist with this ID not found');
+    }
+    return Object.assign(artist, updateArtistDto);
+  }
+  remove(id: string) {
+    if (!id) {
+      throw new BadRequestException('Invalid artist ID provided');
+    }
+    const artist = this.dbService.artists.find((artist) => artist.id === id);
+    if (!artist) {
+      throw new NotFoundException('Artist with this ID not found');
+    }
+    const artistIndex = this.dbService.artists.indexOf(artist);
+    this.dbService.artists.splice(artistIndex, 1);
+
+    this.dbService.tracks
+      .filter((track) => track.artistId === artist.id)
+      .forEach((track) => (track.artistId = null));
+
+    this.dbService.albums
+      .filter((album) => album.artistId === artist.id)
+      .forEach((album) => (album.artistId = null));
+  }
+}
