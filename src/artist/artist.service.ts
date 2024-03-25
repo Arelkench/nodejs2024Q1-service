e88dfa600b30d70de53dcb385367648a2afdf23f
Arelkench <nefera2801@gmail.com>
2024-03-25 19:57:59 +0100

    Refactor artist

--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,70 +1,49 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
-import { DbService } from '../db/db.service';
-import { v4 as uuidv4 } from 'uuid';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateArtistDto } from './dto/create-artist.dto';
-import { Artist } from './types/artist.model';
 import { UpdateArtistDto } from './dto/update-artist.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class ArtistService {
-  constructor(private dbService: DbService) {}
-  create(createArtistDto: CreateArtistDto) {
-    const artist: Artist = {
-      id: uuidv4(),
-      ...createArtistDto,
-    };
-    this.dbService.artists.push(artist);
+  constructor(private prisma: PrismaService) {}
+  async create(createArtistDto: CreateArtistDto) {
+    return this.prisma.artist.create({ data: createArtistDto });
+  }
+  async update(id: string, updateArtistDto: UpdateArtistDto) {
+    const artist = await this.prisma.artist.findUnique({
+      where: { id },
+    });
+
+    if (!artist) {
+      throw new NotFoundException('Artist not found');
+    }
+
+    return this.prisma.artist.update({
+      where: { id },
+      data: updateArtistDto,
+    });
+  }
+  async findAll() {
+    return this.prisma.artist.findMany();
+  }
+  async findOne(id: string) {
+    const artist = await this.prisma.artist.findUnique({
+      where: { id },
+    });
+
+    if (!artist) {
+      throw new NotFoundException('Artist not found');
+    }
+
     return artist;
   }
-  findAll() {
-    return this.dbService.artists;
-  }
-  findOne(id: string) {
-    if (!id) {
-      throw new BadRequestException('Invalid artist ID provided');
-    }
-    const artist = this.dbService.artists.find((artist) => artist.id === id);
-    if (!artist) {
-      throw new NotFoundException('Artist with this ID not found');
-    }
-    return artist;
-  }
-  update(id: string, updateArtistDto: UpdateArtistDto) {
-    const artist = this.dbService.artists.find((artist) => artist.id === id);
-    if (!artist) {
-      throw new NotFoundException('Artist with this ID not found');
-    }
-    return Object.assign(artist, updateArtistDto);
-  }
-  remove(id: string) {
-    if (!id) {
-      throw new BadRequestException('Invalid artist ID provided');
-    }
-    const artist = this.dbService.artists.find((artist) => artist.id === id);
-    if (!artist) {
-      throw new NotFoundException('Artist with this ID not found');
-    }
-    const artistIndex = this.dbService.artists.indexOf(artist);
-    this.dbService.artists.splice(artistIndex, 1);
-
-    this.dbService.tracks
-      .filter((track) => track.artistId === artist.id)
-      .forEach((track) => (track.artistId = null));
-
-    this.dbService.albums
-      .filter((album) => album.artistId === artist.id)
-      .forEach((album) => (album.artistId = null));
-    const artistInFavs = this.dbService.favorites.artists.find(
-      (artist) => artist.id === id,
-    );
-    if (artistInFavs) {
-      const artistIndex =
-        this.dbService.favorites.artists.indexOf(artistInFavs);
-      this.dbService.favorites.artists.splice(artistIndex, 1);
+  async remove(id: string) {
+    try {
+      await this.prisma.artist.delete({
+        where: { id },
+      });
+    } catch {
+      throw new NotFoundException('Artist not found');
     }
   }
 }
