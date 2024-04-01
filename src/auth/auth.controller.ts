1766455669a416210ed6ddc42d17460c2ed9a993
Arelkench <nefera2801@gmail.com>
2024-04-01 15:49:24 +0200

    Auth logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,42 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
-import { CreateUserDto } from '../user/dto/create-user.dto';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { CreateRefreshTokenDto } from './dto/create-refresh-token.dto';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Post('signup')
   signup(
     @Body(new ValidationPipe({ whitelist: true })) userDto: CreateUserDto,
   ) {
-    return;
+    return this.authService.signup(userDto);
   }
 
   @Post('login')
   login(@Body(new ValidationPipe({ whitelist: true })) userDto: CreateUserDto) {
-    return;
+    return this.authService.login(userDto);
   }
 
   @Post('refresh')
-  refresh() {
-    return;
+  @HttpCode(HttpStatus.OK)
+  refresh(
+    @Body(
+      new ValidationPipe({
+        whitelist: true,
+        errorHttpStatusCode: HttpStatus.UNAUTHORIZED,
+      }),
+    )
+    createRefreshTokenDto: CreateRefreshTokenDto,
+  ) {
+    return this.authService.refresh(createRefreshTokenDto);
   }
 }
